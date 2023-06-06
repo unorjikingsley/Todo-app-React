9a97acc60f865d84225990115f2f41bdee2c101f
unorji kingsley <unorjikingsley@gmail.com>
2023-06-06 09:27:10 +0100

    Added a NavBar Heading

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header';
 import TodoLogic from '@/components/TodosLogic';
+import NavBar from '@/routes/NavBar';
 
 import '@/styles/app.css';
 
 const TodoApp = () => (
   <div className="wrapper">
+    <NavBar />
     <div className="todos">
       <Header />
       <TodoLogic />
